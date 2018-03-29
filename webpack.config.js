db32679fd1229ee153d7dc05056e02b00f6454da
Charles Rudolph <charles.rudolph@originate.com>
2018-03-29 13:46:17 -0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
 const path = require('path')
+const BabelEnginePlugin = require('babel-engine-plugin')
 
 process.env.BABEL_ENV = 'browser'
 
 module.exports = {
   externals: {
-    'mz/fs': 'Object'
+    'mz/fs': 'Object',
   },
-  entry: [
-    'babel-polyfill', // To support IE 11
-    path.join(__dirname, 'src', 'index.js')
-  ],
+  entry: path.join(__dirname, 'src', 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'cucumber.js',
@@ -20,23 +18,12 @@ module.exports = {
     rules: [
       {
         loader: 'babel-loader',
-        resource: {
-          and: [
-            { test: /\.js$/ },
-            {
-              include: [
-                // indent-string and figures use the arrow syntax which isn't supported in IE 11
-                path.join(__dirname, 'node_modules', 'indent-string'),
-                path.join(__dirname, 'node_modules', 'figures'),
-
-                path.join(__dirname, 'src'),
-              ],
-            },
-          ],
-        },
+        test: /\.js$/,
+        exclude: /\/node_modules\//,
       },
     ],
   },
+  plugins: [new BabelEnginePlugin()],
   node: {
     child_process: 'empty',
     fs: 'empty',
