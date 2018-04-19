6bbe151c9e3244bc0df0c109d0be8d988fb95ed2
Charles Rudolph <charles.w.rudolph@gmail.com>
2018-04-18 22:17:10 -0500

    update

--- features/support/event_protocol_output_helpers.js
+++ features/support/event_protocol_output_helpers.js
@@ -3,9 +3,5 @@ import path from 'path'
 export function normalizeEventProtocolOutput(str, baseDir) {
   return str
     .replace(/"duration":\d*/g, '"duration":0')
-    .replace(new RegExp('<cwd>', 'g'), baseDir)
-    .replace(
-      /"uri":"([^"]*)"/g,
-      (match, uri) => `"uri":"${path.normalize(uri)}"`
-    )
+    .replace(new RegExp('<cwd>', 'g'), path.posix.normalize(baseDir))
 }
