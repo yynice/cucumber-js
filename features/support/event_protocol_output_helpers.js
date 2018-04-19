3ce36c7609059774520d6e50d3e0ac3b31c3c524
Charles Rudolph <charles.w.rudolph@gmail.com>
2018-04-19 14:18:37 -0500

    update

--- features/support/event_protocol_output_helpers.js
+++ features/support/event_protocol_output_helpers.js
@@ -1,6 +1,10 @@
 export function normalizeEventProtocolOutput(str, baseDir) {
-  const normalizedBaseDir = baseDir.replace(/\\\\/, '/')
+  const normalizedBaseDir = baseDir.replace(/\\\\/g, '/')
   return str
     .replace(/"duration":\d*/g, '"duration":0')
     .replace(/<cwd>/g, normalizedBaseDir)
+    .replace(
+      /"uri":"([^"]*)"/g,
+      (match, uri) => `"uri":"${uri.replace(/\\\\/g, '/')}"`
+    )
 }
